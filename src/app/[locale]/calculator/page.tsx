0e83a5b96281b46c615b02ac6ada9ade19a61024
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2025-03-01 21:59:50 +0100

    add calculator from poland to ukraine

--- src/app/[locale]/calculator/page.tsx
+++ src/app/[locale]/calculator/page.tsx
@@ -1,11 +1,17 @@
 "use client";
+import DeliveryCalculator from '@/components/DeliveryCalculator/DeliveryCalculator';
 import { useParams } from 'next/navigation';
-
+import styles from "./page.module.css";
 const Services = () => {
   const params = useParams();
   console.log('Rendering Services for locale:', params.locale); // locale тепер у params
 
-  return <h1>Сторінка Калькулятор для {params.locale}</h1>;
+  return <>
+   <div className={styles.body}>
+      
+      <DeliveryCalculator />
+   </div>
+  </>
 };
 
 export default Services;
---
+++ src/components/DeliveryCalculator/DeliveryCalculator.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import Image from "next/image";
+import styles from "./DeliveryCalculator.module.css";
+import SvgIcon from "../SvgIcon";
+import { useTranslations } from "next-intl";
+import CalculatorPLtoUA from "./CalculatorPLtoUA/CalculatorPLtoUA";
+
+// import CalculatorPLtoUA from "./CalculatorPLtoUA"; // Калькулятор Польща → Україна
+// import CalculatorUAtoPL from "./CalculatorUAtoPL"; // Калькулятор Україна → Польща
+
+const DeliveryCalculator = () => {
+  const [selectedCalculator, setSelectedCalculator] = useState<"PLtoUA" | "UAtoPL" | null>(null);
+const t = useTranslations('Calculator');
+  return (
+     <div className={styles.container}>
+      
+
+      {!selectedCalculator ? (
+        <>
+          <h1 className={styles.title}>{t("title")}</h1>
+        <p className={styles.description}>{t("description")}</p>
+          <ul className={styles.list}>
+            <li className={styles.listItem}>
+              <button className={styles.link} onClick={() => setSelectedCalculator("PLtoUA")}>
+                {t("fromPoland")} <SvgIcon name="sparow" /> {t("toUkraine")}
+              </button>
+            </li>
+            <li className={styles.listItem}>
+              <button className={styles.link} onClick={() => setSelectedCalculator("UAtoPL")}>
+                {t("fromUkraine")} <SvgIcon name="sparow" /> {t("toPoland")}
+              </button>
+            </li>
+          </ul>
+        </>
+      ) : (
+        <div className={styles.calculatorContainer}>
+          {selectedCalculator === "PLtoUA" && <CalculatorPLtoUA />}
+          {/* {selectedCalculator === "UAtoPL" && <CalculatorUAtoPL />} */}
+          <button className={styles.backButton} onClick={() => setSelectedCalculator(null)}>
+            {t("back")}
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DeliveryCalculator;
