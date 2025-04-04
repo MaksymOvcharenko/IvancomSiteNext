6dc8344f140573e1aefb28a1cfafc8d6dcdb9b20
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2025-04-04 18:14:24 +0200

    add eueu

--- src/app/[locale]/europe-to-europe/page.tsx
+++ src/app/[locale]/europe-to-europe/page.tsx
@@ -1,10 +1,9 @@
-
-"use client"
-import styles from './styles.module.css';
-import Sidebar from '@/components/SideBar/SideBar';
-import HeroServices from '@/components/HeroServices/HeroServices';
-import { FiChevronDown, FiChevronRight } from 'react-icons/fi';
-import EuEuParcels from '@/components/EuEu/EuEuParcels/EuEuParcels';
+"use client";
+import styles from "./styles.module.css";
+import Sidebar from "@/components/SideBar/SideBar";
+import HeroServices from "@/components/HeroServices/HeroServices";
+import { FiChevronDown, FiChevronRight } from "react-icons/fi";
+import EuEuParcels from "@/components/EuEu/EuEuParcels/EuEuParcels";
 
 type Props = {
   searchParams: {
@@ -22,40 +21,49 @@ const EuropeToEuropePage = ({ searchParams }: Props) => {
       </div>
 
       <div className={styles.content}>
-      
-
         <section id="hero" className={styles.section}>
           <HeroServices
-  title="Комфортна та безпечна доставка – наш пріоритет"
-  imageSrc="/image/eueu/hero.png"
-/>
+            title="Комфортна та безпечна доставка – наш пріоритет"
+            imageSrc="/image/eueu/hero.png"
+          />
         </section>
 
         <section id="parcels" className={styles.section}>
-     <details open={openedSection === 'parcels'} className={styles.accordionCont}>
-  <summary className={styles.accordionTitle}>
-    Доставка відправлень між відділеннями Ivancom
-    <FiChevronDown size={32} className={styles.arrow} aria-hidden="true" />
-  </summary>
-  <div className={styles.accordionContent}>
-    <EuEuParcels/>
-  </div>
-</details>
-
-          
+          <details
+            open={openedSection === "parcels"}
+            className={styles.accordionCont}
+          >
+            <summary className={styles.accordionTitle}>
+              Доставка відправлень між відділеннями Ivancom
+              <FiChevronDown
+                size={32}
+                className={styles.arrow}
+                aria-hidden="true"
+              />
+            </summary>
+            <div className={styles.accordionContent}>
+              <EuEuParcels />
+            </div>
+          </details>
         </section>
 
         <section id="move" className={styles.section}>
-          
-          <details open={openedSection === 'move'} className={styles.accordionCont}>
-  <summary className={styles.accordionTitle}>
-    Переїзди
-    <FiChevronDown size={32} className={styles.arrow} aria-hidden="true" />
-  </summary>
-  <div className={styles.accordionContent}>
-    <p>Розкриваючийся аккордеон посилок по Польщі.</p>
-  </div>
-</details>
+          <details
+            open={openedSection === "move"}
+            className={styles.accordionCont}
+          >
+            <summary className={styles.accordionTitle}>
+              Переїзди
+              <FiChevronDown
+                size={32}
+                className={styles.arrow}
+                aria-hidden="true"
+              />
+            </summary>
+            <div className={styles.accordionContent}>
+              <p>Розкриваючийся аккордеон посилок по Польщі.</p>
+            </div>
+          </details>
         </section>
       </div>
     </div>
--- src/components/EuEu/EuEuParcels/EuEuParcels.tsx
+++ src/components/EuEu/EuEuParcels/EuEuParcels.tsx
@@ -1,120 +1,232 @@
-"use client";
+// "use client";
+
+// import { LiaCoinsSolid } from "react-icons/lia";
+// import s from "./EuEuParcels.module.css";
+// import { CiClock1 } from "react-icons/ci";
+// import { FaRegClock } from "react-icons/fa6";
+
+// import { useParams } from "next/navigation";
+// import Link from "next/link";
+
+// const EuEuParcels = () => {
+//   const { locale } = useParams();
+//   return (
+//     <div className={s.body}>
+//       <div className={s.header}>
+//         <p className={s.descr}>
+//           Пересилайте посилки швидко та зручно між стаіонарними відділеннями
+//           IVANCOM по всій Польщі! Надійна доставка без зайвих затримок – просто
+//           принесіть посилку у найближче відділення, а ми подбаємо про решту.
+//         </p>
+//         <div className={s.contHeader}>
+//           <div className={s.tariffs}>
+//             <h3 className={s.tarifTitle}>
+//               <LiaCoinsSolid size={30} />
+//               Тариф
+//             </h3>
+//             <p className={s.tarifDesc}>3 зл/кг фактичної або об`ємної ваги</p>
+//             <ul className={s.tarifList}>
+//               <li className={s.item}>
+//                 <p className={s.itemTitle}>Страховий платіж </p>
+//                 <p className={s.itemDesc}>1% від оціночної вартості</p>
+//               </li>
+//               <li className={s.item}>
+//                 <p className={s.itemTitle}>Мінімальна вартість</p>
+//                 <p className={s.itemDesc}>
+//                   40 зл до 5 кг фактичної або об`ємної ваги
+//                 </p>
+//               </li>
+//             </ul>
+//           </div>
+//           <div className={s.tariffs}>
+//             <h3 className={s.tarifTitle}>
+//               <FaRegClock size={26} />
+//               Термін
+//             </h3>
+//             <p className={s.tarifDesc}>Термін доставки від 1 до 8 днів</p>
+//           </div>
+//         </div>
+//       </div>
+//       <div className={s.price}>
+//         <h3 className={s.priceTitle}>Відділення далеко? Це не проблема!</h3>
+//         <p className={s.priceDescr}>
+//           Викличте кур’єра IVANCOM, і ми самі подбаємо про все. Зручність і
+//           швидкість – це те, що ми гарантуємо!
+//         </p>
+//         <div className={s.priceTable}>
+//           <h4 className={s.tableTitle}>
+//             Доставка з відділень в Польщі до партнерських відділень в Україні
+//           </h4>
+//           <ul className={s.tableList}>
+//             <li className={s.tableItem}>
+//               <div className={s.firstColumn}>Краків</div>
+//               <div className={s.secondColumn}>Від 40 zł</div>
+//             </li>
+//             <li className={s.tableItem}>
+//               <div className={s.firstColumn}>Варшава</div>
+//               <div className={s.secondColumn}>Від 30 zł</div>
+//             </li>
+//             <li className={s.tableItem}>
+//               <div className={s.firstColumn}>Катовіце</div>
+//               <div className={s.secondColumn}>Від 30 zł</div>
+//             </li>
+//             <li className={s.tableItem}>
+//               <div className={s.firstColumn}>Вроцлав</div>
+//               <div className={s.secondColumn}>Від 30 zł</div>
+//             </li>
+//             <li className={s.tableItem}>
+//               <div className={s.firstColumn}>Кельце</div>
+//               <div className={s.secondColumn}>Безкоштовно</div>
+//             </li>
+//           </ul>
+//           <Link href={`/${locale}/contacts`} className={s.priceBtn}>
+//             Контакти
+//           </Link>
+//         </div>
+//       </div>
+//       <div className={s.steps}>
+//         <h3 className={s.stepTitleMain}>
+//           Відправляй просто, як раз, два, три!
+//         </h3>
+//         <ul className={s.stepsList}>
+//           <li className={s.stepItem}>
+//             <div className={s.step}>1</div>
+//             <div className={s.steptext}>
+//               <p className={s.stepTitle}>Запакуй</p>
+//               <p className={s.stepDescr}>
+//                 Запаковуй свою посилку так, як зручно тобі!
+//               </p>
+//             </div>
+//           </li>
+//           <li className={s.stepItem}>
+//             <div className={s.step}>2</div>
+//             <div className={s.steptext}>
+//               <p className={s.stepTitle}>Відправ</p>
+//               <p className={s.stepDescr}>
+//                 Принеси посилку, здійсни оплати – і вона вже в дорозі!
+//               </p>
+//             </div>
+//           </li>
+//           <li className={s.stepItem}>
+//             <div className={s.step}>3</div>
+//             <div className={s.steptext}>
+//               <p className={s.stepTitle}>Ні про що не хвилюйся</p>
+//               <p className={s.stepDescr}>
+//                 Отримувач отримує смс коли його посилка прибуде до відділення
+//                 IVANCOM
+//               </p>
+//             </div>
+//           </li>
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default EuEuParcels;
+
 
 import { LiaCoinsSolid } from "react-icons/lia";
-import s from "./EuEuParcels.module.css";
 import { CiClock1 } from "react-icons/ci";
 import { FaRegClock } from "react-icons/fa6";
-
 import { useParams } from "next/navigation";
+import { useTranslations } from "next-intl";
 import Link from "next/link";
+import s from "./EuEuParcels.module.css";
 
 const EuEuParcels = () => {
   const { locale } = useParams();
+  const t = useTranslations("EuEU.parcels");
+
   return (
     <div className={s.body}>
       <div className={s.header}>
-        <p className={s.descr}>
-          Пересилайте посилки швидко та зручно між стаіонарними відділеннями
-          IVANCOM по всій Польщі! Надійна доставка без зайвих затримок – просто
-          принесіть посилку у найближче відділення, а ми подбаємо про решту.
-        </p>
+        <p className={s.descr}>{t("header.descr")}</p>
         <div className={s.contHeader}>
           <div className={s.tariffs}>
             <h3 className={s.tarifTitle}>
               <LiaCoinsSolid size={30} />
-              Тариф
+              {t("tariff.title")}
             </h3>
-            <p className={s.tarifDesc}>3 зл/кг фактичної або об`ємної ваги</p>
+            <p className={s.tarifDesc}>{t("tariff.price")}</p>
             <ul className={s.tarifList}>
               <li className={s.item}>
-                <p className={s.itemTitle}>Страховий платіж </p>
-                <p className={s.itemDesc}>1% від оціночної вартості</p>
+                <p className={s.itemTitle}>{t("tariff.insurance.title")}</p>
+                <p className={s.itemDesc}>{t("tariff.insurance.desc")}</p>
               </li>
               <li className={s.item}>
-                <p className={s.itemTitle}>Мінімальна вартість</p>
-                <p className={s.itemDesc}>
-                  40 зл до 5 кг фактичної або об`ємної ваги
-                </p>
+                <p className={s.itemTitle}>{t("tariff.min.title")}</p>
+                <p className={s.itemDesc}>{t("tariff.min.desc")}</p>
               </li>
             </ul>
           </div>
+
           <div className={s.tariffs}>
             <h3 className={s.tarifTitle}>
               <FaRegClock size={26} />
-              Термін
+              {t("delivery.title")}
             </h3>
-            <p className={s.tarifDesc}>Термін доставки від 1 до 8 днів</p>
+            <p className={s.tarifDesc}>{t("delivery.time")}</p>
           </div>
         </div>
       </div>
+
       <div className={s.price}>
-        <h3 className={s.priceTitle}>Відділення далеко? Це не проблема!</h3>
-        <p className={s.priceDescr}>
-          Викличте кур’єра IVANCOM, і ми самі подбаємо про все. Зручність і
-          швидкість – це те, що ми гарантуємо!
-        </p>
+        <h3 className={s.priceTitle}>{t("price.title")}</h3>
+        <p className={s.priceDescr}>{t("price.descr")}</p>
+
         <div className={s.priceTable}>
-          <h4 className={s.tableTitle}>
-            Доставка з відділень в Польщі до партнерських відділень в Україні
-          </h4>
+          <h4 className={s.tableTitle}>{t("price.tableTitle")}</h4>
           <ul className={s.tableList}>
             <li className={s.tableItem}>
-              <div className={s.firstColumn}>Краків</div>
-              <div className={s.secondColumn}>Від 40 zł</div>
+              <div className={s.firstColumn}>{t("cities.krakow")}</div>
+              <div className={s.secondColumn}>{t("cities.krakowPrice")}</div>
             </li>
             <li className={s.tableItem}>
-              <div className={s.firstColumn}>Варшава</div>
-              <div className={s.secondColumn}>Від 30 zł</div>
+              <div className={s.firstColumn}>{t("cities.warsaw")}</div>
+              <div className={s.secondColumn}>{t("cities.warsawPrice")}</div>
             </li>
             <li className={s.tableItem}>
-              <div className={s.firstColumn}>Катовіце</div>
-              <div className={s.secondColumn}>Від 30 zł</div>
+              <div className={s.firstColumn}>{t("cities.katowice")}</div>
+              <div className={s.secondColumn}>{t("cities.katowicePrice")}</div>
             </li>
             <li className={s.tableItem}>
-              <div className={s.firstColumn}>Вроцлав</div>
-              <div className={s.secondColumn}>Від 30 zł</div>
+              <div className={s.firstColumn}>{t("cities.wroclaw")}</div>
+              <div className={s.secondColumn}>{t("cities.wroclawPrice")}</div>
             </li>
             <li className={s.tableItem}>
-              <div className={s.firstColumn}>Кельце</div>
-              <div className={s.secondColumn}>Безкоштовно</div>
+              <div className={s.firstColumn}>{t("cities.kielce")}</div>
+              <div className={s.secondColumn}>{t("cities.kielcePrice")}</div>
             </li>
           </ul>
           <Link href={`/${locale}/contacts`} className={s.priceBtn}>
-            Контакти
+            {t("price.contacts")}
           </Link>
         </div>
       </div>
+
       <div className={s.steps}>
-        <h3 className={s.stepTitleMain}>
-          Відправляй просто, як раз, два, три!
-        </h3>
+        <h3 className={s.stepTitleMain}>{t("steps.title")}</h3>
         <ul className={s.stepsList}>
           <li className={s.stepItem}>
             <div className={s.step}>1</div>
             <div className={s.steptext}>
-              <p className={s.stepTitle}>Запакуй</p>
-              <p className={s.stepDescr}>
-                Запаковуй свою посилку так, як зручно тобі!
-              </p>
+              <p className={s.stepTitle}>{t("steps.step1.title")}</p>
+              <p className={s.stepDescr}>{t("steps.step1.desc")}</p>
             </div>
           </li>
           <li className={s.stepItem}>
             <div className={s.step}>2</div>
             <div className={s.steptext}>
-              <p className={s.stepTitle}>Відправ</p>
-              <p className={s.stepDescr}>
-                Принеси посилку, здійсни оплати – і вона вже в дорозі!
-              </p>
+              <p className={s.stepTitle}>{t("steps.step2.title")}</p>
+              <p className={s.stepDescr}>{t("steps.step2.desc")}</p>
             </div>
           </li>
           <li className={s.stepItem}>
             <div className={s.step}>3</div>
             <div className={s.steptext}>
-              <p className={s.stepTitle}>Ні про що не хвилюйся</p>
-              <p className={s.stepDescr}>
-                Отримувач отримує смс коли його посилка прибуде до відділення
-                IVANCOM
-              </p>
+              <p className={s.stepTitle}>{t("steps.step3.title")}</p>
+              <p className={s.stepDescr}>{t("steps.step3.desc")}</p>
             </div>
           </li>
         </ul>
