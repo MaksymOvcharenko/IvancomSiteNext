e92a546550d1ba855cd8fa082f53cf2c13b7082b
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2025-04-09 16:19:56 +0200

    change

--- src/components/UaEu/SelectorTariffs/PlatinumTariff/PlatinumPl/table.ts
+++ src/components/UaEu/SelectorTariffs/PlatinumTariff/PlatinumPl/table.ts
@@ -1,48 +1,48 @@
-import { DeliveryTableData } from "@/components/DeliveryTable/DeliveryTable";
+// import { DeliveryTableData } from "@/components/DeliveryTable/DeliveryTable";
 
-import { useTranslations } from 'next-intl';
-const t = useTranslations("UaEu.ParcelsUaEu.Silver.Table");
-
-
+// import { useTranslations } from 'next-intl';
+// const t = useTranslations("UaEu.ParcelsUaEu.Silver.Table");
 
 
-export const deliveryIvankomPL: DeliveryTableData = {
-  title: t('tables.ivankomBranch'),
-  country:  t('country.pl'),
-  rows: [
-    { weight: "до 2 кг", price: "40 zł" },
-    { weight: "2–5 кг", price: "60 zł" },
-    { weight: "5–10 кг", price: "80 zł" },
-    { weight: "10–15 кг", price: "100 zł" },
-    { weight: "15–20 кг", price: "120 zł" },
-    { weight: "20–30 кг", price: "150 zł" },
-    { weight: "понад 30 кг", price: "5 zł / кг" },
-  ],
+
+
+// export const deliveryIvankomPL: DeliveryTableData = {
+//   title: t('tables.ivankomBranch'),
+//   country:  t('country.pl'),
+//   rows: [
+//     { weight: "до 2 кг", price: "40 zł" },
+//     { weight: "2–5 кг", price: "60 zł" },
+//     { weight: "5–10 кг", price: "80 zł" },
+//     { weight: "10–15 кг", price: "100 zł" },
+//     { weight: "15–20 кг", price: "120 zł" },
+//     { weight: "20–30 кг", price: "150 zł" },
+//     { weight: "понад 30 кг", price: "5 zł / кг" },
+//   ],
   
-};
+// };
 
-export const courierIvankomPrices: DeliveryTableData = {
-  title: t('tables.courier'),
-  country: t('country.pl'),
-  rows: [
-    { weight: "Краків", price: "від 40 zł" },
-    { weight: "Варшава", price: "від 30 zł" },
-    { weight: "Катовіце", price: "від 30 zł" },
-    { weight: "Вроцлав", price: "від 30 zł" },
-    { weight: "Кельце", price: "у місті безкоштовно" },
-  ],
-};
+// export const courierIvankomPrices: DeliveryTableData = {
+//   title: t('tables.courier'),
+//   country: t('country.pl'),
+//   rows: [
+//     { weight: "Краків", price: "від 40 zł" },
+//     { weight: "Варшава", price: "від 30 zł" },
+//     { weight: "Катовіце", price: "від 30 zł" },
+//     { weight: "Вроцлав", price: "від 30 zł" },
+//     { weight: "Кельце", price: "у місті безкоштовно" },
+//   ],
+// };
 
-export const inpostDeliveryPL: DeliveryTableData = {
-  title: t('tables.inpost'),
-  country:  t('country.pl'),
-  rows: [
-    { weight: "до 2 кг", price: "60 zł" },
-    { weight: "2–5 кг", price: "80 zł" },
-    { weight: "5–10 кг", price: "120 zł" },
-    { weight: "10–15 кг", price: "150 zł" },
-    { weight: "20–30 кг", price: "190 zł" },
-    { weight: "понад 30 кг", price: "7 zł / кг" },
-  ],
-  notes:  [t('tables.inpostNote')],
-};
+// export const inpostDeliveryPL: DeliveryTableData = {
+//   title: t('tables.inpost'),
+//   country:  t('country.pl'),
+//   rows: [
+//     { weight: "до 2 кг", price: "60 zł" },
+//     { weight: "2–5 кг", price: "80 zł" },
+//     { weight: "5–10 кг", price: "120 zł" },
+//     { weight: "10–15 кг", price: "150 zł" },
+//     { weight: "20–30 кг", price: "190 zł" },
+//     { weight: "понад 30 кг", price: "7 zł / кг" },
+//   ],
+//   notes:  [t('tables.inpostNote')],
+// };
