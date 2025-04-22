fa08e2f4943aee45a12263ab695f49075a51deec
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2025-04-22 10:28:16 +0200

    add Change in Title and FavIcon

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -33,10 +33,15 @@ export default async function RootLayout({
   return (
     <html lang={locale}>
       <head>
+          <title>IVANCOM</title>
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Rounded+Mplus+1c:wght@300;400;500;700&family=Inter:wght@300;400;500;700&family=Open+Sans:wght@300;400;600&family=Montserrat:wght@300;400;500;600;700&display=swap"
         />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+<link rel="manifest" href="/site.webmanifest"/>
       </head>
       <body className={inter.className}>
         <NextIntlClientProvider locale={locale} messages={messages}>
