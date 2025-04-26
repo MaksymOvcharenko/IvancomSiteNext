a380827b42e0149797082f65bdefd284080ce3e3
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2025-04-26 19:09:26 +0200

    add pixel

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -26,7 +26,7 @@ export default async function RootLayout({
   if (!availableLocales.includes(locale)) {
     notFound();
   }
-
+  
   // ОТРИМУЄМО ПЕРЕКЛАДИ
   const messages = (await import(`../../../messages/${locale}.json`)).default;
 
@@ -41,7 +41,27 @@ export default async function RootLayout({
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-<link rel="manifest" href="/site.webmanifest"/>
+        <link rel="manifest" href="/site.webmanifest" />
+        <script dangerouslySetInnerHTML={{ __html: `
+  if (!window.fbq) {
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '689234196807365');
+    fbq('track', 'PageView');
+  }
+`}} />
+
+  <noscript>
+    <img height="1" width="1" style={{display: "none"}}
+      src="https://www.facebook.com/tr?id=689234196807365&ev=PageView&noscript=1"
+    />
+  </noscript>
       </head>
       <body className={inter.className}>
         <NextIntlClientProvider locale={locale} messages={messages}>
